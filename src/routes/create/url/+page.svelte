1a852364e91d99d7446030d171b2de3c448c9da4
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-06-20 14:24:11 +0900

    upgrade to plus added for import

--- src/routes/create/url/+page.svelte
+++ src/routes/create/url/+page.svelte
@@ -2,6 +2,7 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
   import { page } from "$app/stores";
+  import { user } from "$lib/services/auth.js";
   import axiosAPI from "$lib/services/customAxios.js";
   import {
     Label,
@@ -11,6 +12,7 @@
     Alert,
     A,
     Select,
+    P,
   } from "flowbite-svelte";
 
   //import type { PageData } from './$types';
@@ -18,7 +20,6 @@
   import SuperDebug from "sveltekit-superforms/client/SuperDebug.svelte";
 
   export let data; // : PageData
-
   //   select data
   let countries = [
     { value: 1, name: "Everyone" },
@@ -30,7 +31,7 @@
     {
       resetForm: true,
       delayMs: 1,
-      timeoutMs:10,
+      timeoutMs: 10,
     }
   );
 
@@ -45,68 +46,72 @@
   {#if $delayed}<span class="delayed">Working...</span>{/if}
   {#if $submitting}<span class="delayed">Working2...</span>{/if}
 
-  <form method="POST" use:enhance class="mb-9">
-    {#if $message}
-      <Alert dismissable>
-        {$message}
+  {#if $user && $user?.expires_on == null}
+    <P>Please upgrade to plus <A href="/profile">from here</A></P>
+  {:else}
+    <form method="POST" use:enhance class="mb-9">
+      {#if $message}
+        <Alert dismissable>
+          {$message}
+        </Alert>
+      {/if}
+      <Alert color="dark" class="mb-3">
+        You can easily import vocabulary sets or folders from <A
+          href="https://quizlet.com"
+          rel="external"
+          target="_blank"
+          class="font-medium hover:underline">quizlet.com</A
+        >, <A
+          href="https://vocabulary.com"
+          rel="external"
+          target="_blank"
+          class="font-medium hover:underline">vocabulary.com</A
+        >, or <A
+          href="https://memrise.com"
+          rel="external"
+          target="_blank"
+          class="font-medium hover:underline">memrise.com</A
+        >. Simply copy and paste the URL of the set or folder you want to
+        import, and we'll take care of the rest for you.
       </Alert>
-    {/if}
-    <Alert color="dark" class="mb-3">
-      You can easily import vocabulary sets or folders from <A
-        href="https://quizlet.com"
-        rel="external"
-        target="_blank"
-        class="font-medium hover:underline">quizlet.com</A
-      >, <A
-        href="https://vocabulary.com"
-        rel="external"
-        target="_blank"
-        class="font-medium hover:underline">vocabulary.com</A
-      >, or <A
-        href="https://memrise.com"
-        rel="external"
-        target="_blank"
-        class="font-medium hover:underline">memrise.com</A
-      >. Simply copy and paste the URL of the set or folder you want to import,
-      and we'll take care of the rest for you.
-    </Alert>
 
-    <div class="mb-6">
-      <Label
-        for="url"
-        color={$errors.url ? "red" : undefined}
-        class="block mb-2">URL</Label
-      >
-      <Input
-        id="url"
-        color={$errors.url ? "red" : undefined}
-        placeholder="https://quizlet.com/saint1729/folders/gregmat/sets"
-        name="url"
-        bind:value={$form.url}
-        type="url"
-        {...$constraints.url}
-      />
-      {#if $errors.url}
-        <Helper class="mt-2" color="red">
-          <span class="font-bold">{$errors.url}</span></Helper
-        >{/if}
-    </div>
-
-    <div class="mb-6">
-      <Label
-        >Visible to
-        <Select
-          class="mt-2"
-          name="visibility"
-          items={countries}
-          bind:value={$form.visibility}
-          {...$constraints.visibility}
+      <div class="mb-6">
+        <Label
+          for="url"
+          color={$errors.url ? "red" : undefined}
+          class="block mb-2">URL</Label
+        >
+        <Input
+          id="url"
+          color={$errors.url ? "red" : undefined}
+          placeholder="https://quizlet.com/saint1729/folders/gregmat/sets"
+          name="url"
+          bind:value={$form.url}
+          type="url"
+          {...$constraints.url}
         />
-      </Label>
-    </div>
+        {#if $errors.url}
+          <Helper class="mt-2" color="red">
+            <span class="font-bold">{$errors.url}</span></Helper
+          >{/if}
+      </div>
 
-    <Button type="submit">Import data</Button>
-  </form>
+      <div class="mb-6">
+        <Label
+          >Visible to
+          <Select
+            class="mt-2"
+            name="visibility"
+            items={countries}
+            bind:value={$form.visibility}
+            {...$constraints.visibility}
+          />
+        </Label>
+      </div>
+
+      <Button type="submit">Import data</Button>
+    </form>
+  {/if}
 </div>
 
 <style>
