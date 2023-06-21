27c0f496e5a5bfcb3be57a017f7263171e071f9c
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2023-06-21 14:29:45 +0900

    create folder fixed

--- src/routes/(authenticated)/create/folder/+page.svelte
+++ src/routes/(authenticated)/create/folder/+page.svelte
@@ -1,22 +1,83 @@
 <!-- @format -->
 <script lang="ts">
-  import { enhance } from "$app/forms";
-  import { page } from "$app/stores";
+  import { z } from "zod";
   import {
     Label,
     Helper,
     Input,
     Button,
     Alert,
-    P,
     Select,
+    Heading,
   } from "flowbite-svelte";
 
-  import type { PageData } from "./$types";
-  import { superForm } from "sveltekit-superforms/client";
-  import SuperDebug from "sveltekit-superforms/client/SuperDebug.svelte";
+  import DevComponent from "$lib/components/DevComponent.svelte";
+  import { onMount } from "svelte";
+  import CloseIcon from "$lib/icons/closeIcon.svelte";
+  import axiosAPI from "$lib/services/customAxios";
+  import type { AxiosError, AxiosResponse } from "axios";
 
-  export let data: PageData; // : PageData
+  interface FormData {
+    name: string;
+    visibility: number;
+  }
+
+  interface FormErrors {
+    name?: string | null;
+    visibility?: string | null;
+  }
+
+  interface FolderCreateSuccessResponse {
+    data: Data;
+    message: string;
+  }
+
+  interface Data {
+    id: number;
+    user_id: number;
+    list_meta_id: null;
+    name: string;
+    slug: string;
+    visibility: number;
+    status: number;
+    crated_at: Date;
+    updated_at: Date;
+  }
+
+  interface FolderCreateErrorResponse {
+    errors?: Errors;
+  }
+
+  interface Errors {
+    name?: string;
+    visibility?: string;
+  }
+
+  const schema = z.object({
+    name: z.string().nonempty({ message: "Folder name is required." }),
+    visibility: z.number().default(1),
+  });
+
+  // form errors
+  let FormErrors: FormErrors = {
+    name: null,
+    visibility: null,
+  };
+  let formError: string | null = null;
+  let formSuccess: string | null = null;
+
+  // form data
+  let formData: FormData = {
+    name: "",
+    visibility: 1,
+  };
+  let submitting = true;
+  let resetForm: HTMLFormElement;
+
+  onMount(() => {
+    submitting = false;
+    resetForm = <HTMLFormElement>document.getElementById("form");
+  });
 
   //   select data
   let visibility = [
@@ -24,54 +85,114 @@
     { value: 2, name: "Only me" },
   ];
 
-  const { form, submitting, delayed, timeout, message, errors, constraints } =
-    superForm(data.form, {
-      resetForm: true,
-      onResult: (event) => {
-        console.log(event)
-      },
-      
-    });
-    function test() {
-      console.log('test')
+  async function handleSubmit() {
+    submitting = true;
+    // first clear the errors
+    resetFormErrors();
+    const result = schema.safeParse(formData);
+    console.log(result);
+
+    if (!result.success) {
+      const formatted = result.error.format();
+      console.log(formatted);
+      // now set the errors
+      FormErrors.name = formatted.name?._errors[0] ?? null;
+      FormErrors.visibility = formatted.visibility?._errors[0] ?? null;
+    } else {
+      // now set the errors
+      // now submit the data
+      console.log("data submit");
+      console.log(result.data);
+
+      await axiosAPI
+        .post("/folders", result.data)
+        .then((res: AxiosResponse) => {
+          if (res.status == 201) {
+            resetForm.reset();
+            const responseData: FolderCreateSuccessResponse = res.data;
+            formSuccess = responseData.message;
+          } else {
+            formError = "Some error occurred.";
+          }
+        })
+        .catch((err) => {
+          if (err.response?.status == 422) {
+            // validation error
+            const d: FolderCreateErrorResponse = err.response.data;
+
+            if (d.errors?.name) {
+              FormErrors.name = d.errors.name;
+            }
+            if (d.errors?.visibility) {
+              FormErrors.visibility = d.errors.visibility;
+            }
+          } else {
+            formError = err.response.data.errors;
+          }
+        })
+        .finally(() => {
+          submitting = false;
+        });
     }
 
+    submitting = false;
+  }
 
+  function resetFormErrors() {
+    FormErrors.name = null;
+    FormErrors.visibility = null;
+    formError = null;
+  }
 </script>
 
 <div class="mt-5">
-  <SuperDebug data={$form} />
-  <P size="4xl" weight="medium">Create Folder</P>
-  {$submitting}
-  {$delayed}
-  {$timeout}
+  <DevComponent>
+    <p>Name: {formData.name}</p>
+    <p>visibility: {formData.visibility}</p>
+  </DevComponent>
+  <div class="my-3">
+    <Heading tag="h4">Create Folder</Heading>
+  </div>
+  {#if formError}
+    <Alert color="red" dismissable class="my-1">
+      <span slot="icon"><CloseIcon /></span>
+      {formError}
+    </Alert>
+  {/if}
 
-  <form method="POST" use:enhance class="mb-9 mt-3">
-    {#if $message}
-      <Alert dismissable>
-        {$message}
-      </Alert>
-    {/if}
+  {#if formSuccess}
+    <Alert color="green" dismissable class="my-1">
+      <span slot="icon"><CloseIcon /></span>
+      {formSuccess}
+    </Alert>
+  {/if}
 
+  <form id="form" on:submit|preventDefault={handleSubmit} class="mb-9 mt-3">
     <div class="mb-6">
       <Label
-        for="url"
-        color={$errors.name ? "red" : undefined}
-        class="block mb-2">Folder Name</Label
+        for="name"
+        class="block mb-2"
+        color={FormErrors.name ? "red" : undefined}>Folder Name</Label
       >
       <Input
         id="name"
-        color={$errors.name ? "red" : undefined}
+        required
         placeholder="GregMat Vocabulary Lists"
+        color={FormErrors.name ? "red" : undefined}
         name="name"
-        bind:value={$form.name}
+        bind:value={formData.name}
         type="text"
-        {...$constraints.name}
       />
-      {#if $errors.name}
+      {#if FormErrors.name}
+        <Helper class="mt-2 text-sm" helperClass="text-lg" color="red">
+          {FormErrors.name}
+        </Helper>
+      {/if}
+      {#if FormErrors.visibility}
         <Helper class="mt-2" color="red">
-          <span class="font-bold">{$errors.name}</span></Helper
-        >{/if}
+          {FormErrors.visibility}
+        </Helper>
+      {/if}
     </div>
 
     <div class="mb-6">
@@ -81,12 +202,11 @@
           class="mt-2"
           name="visibility"
           items={visibility}
-          bind:value={$form.visibility}
-          {...$constraints.visibility}
+          bind:value={formData.visibility}
         />
       </Label>
     </div>
 
-    <Button type="submit">Create Folder</Button>
+    <Button type="submit" disabled={submitting}>Create Folder</Button>
   </form>
 </div>
